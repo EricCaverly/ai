e9ae2d07e56fb46c15265af90542784e3375dae1
EricCaverly <ecaverly1@nait.ca>
2024-12-20 17:02:00 -0700

    Added documentation. Changed split-user audio tracking from map to slice so it remains in order (FIFO)

--- discord/bot.go
+++ discord/bot.go
@@ -8,87 +8,113 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-type VoiceSessions struct {
-	vc   []*discordgo.VoiceConnection
-	mutx sync.Mutex
+const (
+	bot_msg_prefix = '!'
+)
+
+type VoiceSession struct {
+	connection *discordgo.VoiceConnection
+	guild_id   string
 }
 
 var (
-	Calls        VoiceSessions
+	calls        []VoiceSession
+	calls_mutx   sync.Mutex
 	ssrc_to_user = make(map[uint32]string)
 )
 
+// Message Created Handler
+// - Called when a message is sent in any guild
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Dont do anything to messages sent by botself
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
+	// If the message does not have any content (images for example) do nothing
 	if len(m.Content) == 0 {
 		return
 	}
 
-	if m.Content[0] != '!' {
+	// If the first character is not the bot message prefix, ignore
+	if m.Content[0] != bot_msg_prefix {
 		return
 	}
 
+	// Switch based on the command
 	switch m.Content[1:] {
 	case "join":
 		join_voice(s, m)
 	case "leave":
-		leave_voice()
+		leave_voice(m.GuildID)
 	}
 }
 
-func leave_voice() {
-	Calls.mutx.Lock()
-	for _, c := range Calls.vc {
-		c.Disconnect()
-		close(c.OpusRecv)
-		c.Close()
-
+func leave_voice(guild_id string) {
+	calls_mutx.Lock()
+	// Find the voice call which relates to the guild the message was sent in
+	var i int
+	for i < len(calls) {
+		// If we found it, leave the voice call, destroying the call object
+		if calls[i].guild_id == guild_id {
+			calls[i].connection.Disconnect()
+			close(calls[i].connection.OpusRecv)
+			calls[i].connection.Close()
+			calls = append(calls[:i], calls[i+1:]...)
+		}
+		i++
 	}
-	Calls.vc = []*discordgo.VoiceConnection{}
-	Calls.mutx.Unlock()
+	calls_mutx.Unlock()
 }
 
 func join_voice(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	// Find which voice channel the author of the message is in
 	g, vs, err := find_vc(s, m)
 	if err != nil {
 		log.Printf("Problem finding VC\n")
 		return
 	}
 
+	// Join that voice call
 	vc, err := s.ChannelVoiceJoin(g.ID, vs.ChannelID, true, false)
 	if err != nil {
 		log.Printf("Failed to join VC: %s\n", err.Error())
 	}
 
-	Calls.mutx.Lock()
-	Calls.vc = append(Calls.vc, vc)
-	Calls.mutx.Unlock()
+	// Make a new entry in the calls slice containing a reference to the call
+	calls_mutx.Lock()
+	calls = append(calls, VoiceSession{
+		connection: vc,
+		guild_id:   g.ID,
+	})
+	calls_mutx.Unlock()
 
+	// Begin handling voice call related logic for transcription
 	handleVoice(s, m.ChannelID, vc.OpusRecv)
 }
 
 func find_vc(s *discordgo.Session, m *discordgo.MessageCreate) (*discordgo.Guild, *discordgo.VoiceState, error) {
-	// Find Chanel
+	// Find Chanel the message was sent in
 	c, err := s.State.Channel(m.ChannelID)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// Find guild
+	// Find guild the channel is within
 	g, err := s.State.Guild(c.GuildID)
 	if err != nil {
 		return nil, nil, err
 	}
 
+	// Find voice state the author is in
 	for _, vs := range g.VoiceStates {
+
+		// If the author is in a voice call, return a reference to it
 		if vs.UserID == m.Author.ID {
 			return g, vs, nil
 		}
 	}
 
+	// Otherwise, return an error
 	return nil, nil, fmt.Errorf("unable to find voice call")
 }
--- discord/stt.go
+++ discord/stt.go
@@ -40,6 +40,7 @@ func createPionRTPPacket(p *discordgo.Packet) *rtp.Packet {
 type Speech struct {
 	file    media.Writer
 	length  int
+	ssrc    uint32
 	endTime time.Time
 }
 
@@ -49,38 +50,51 @@ type WhisperResp struct {
 }
 
 func handleVoice(s *discordgo.Session, chan_id string, discord_packets chan *discordgo.Packet) {
-	users := map[uint32]Speech{}
+	users := []Speech{}
 	var u_mut sync.Mutex
 	var running bool = true
 
 	go func() {
 		for running {
 			u_mut.Lock()
-			for usr := range users {
-				if users[usr].endTime.After(time.Now()) {
+			// Go over each user is currently speaking users
+			for i := range users {
+				// If their sentance has not timed out, skip
+				if users[i].endTime.After(time.Now()) {
 					continue
 				}
-				log.Printf("user %d stopped talking", usr)
-				users[usr].file.Close()
 
-				if users[usr].length > 10 {
-					whisper_data, err := send_speech_to_whisper(usr)
+				// Otherwise, they have not made noise for sentance_end_time_ms and are probably done talking
+				log.Printf("user %d stopped talking", i)
+
+				// Close the OGG file
+				users[i].file.Close()
+
+				// If the number of audio packets is greater than 10, this is probably real data
+				if users[i].length > 10 {
+					// Call the send function, which will call the whisper API, returning transcription data
+					whisper_data, err := send_speech_to_whisper(users[i].ssrc)
 					if err != nil {
 						log.Printf("problem sending data to whisper: %s\n", err.Error())
 					}
 
+					// If the length of the transcription is not 0, actual text was received
 					if len(whisper_data.Transcription) > 0 {
 						fmt.Printf("%s : %s\n", whisper_data.Username, whisper_data.Transcription)
 						s.ChannelMessageSend(chan_id, whisper_data.Transcription)
+						// Otherwise, if the length is 0, likely it was just hot mic, and should not be sent to LLM
 					} else {
 						fmt.Println("empty transcription returned")
 					}
-
 				}
 
-				os.Remove(fmt.Sprintf("%d.ogg", usr))
-				delete(users, usr)
+				// Remove the OGG file
+				os.Remove(fmt.Sprintf("%d.ogg", i))
+
+				// Remove the member from the actively speaking users slice
+				users = append(users[:i], users[i+1:]...)
 			}
+
 			u_mut.Unlock()
 		}
 	}()
@@ -91,31 +105,53 @@ func handleVoice(s *discordgo.Session, chan_id string, discord_packets chan *dis
 		}
 
 		u_mut.Lock()
-		speech, ok := users[p.SSRC]
-		if !ok {
+
+		// See if the user who is talking has already been talking
+		// If i is never changed from -1, we know this is a user who just started talking
+		var i int = -1
+		for j := range users {
+			if users[j].ssrc == p.SSRC {
+				i = j
+			}
+		}
+
+		// Create a new entry for this user who just started talking
+		if i == -1 {
 			log.Printf("user %d started talking\n", p.SSRC)
+			// Create new member of users talking slice
+			users = append(users, Speech{
+				endTime: time.Now().Add(500 * time.Millisecond),
+				ssrc:    p.SSRC,
+				length:  0,
+			})
+			i = len(users) - 1
+
+			// Open a new OGG file
 			var err error
-			speech.endTime = time.Now().Add(1 * time.Second)
-			speech.file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 2)
+			users[i].file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 2)
 			if err != nil {
 				fmt.Printf("failed to create file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
 				return
 			}
-			users[p.SSRC] = speech
 		}
+
 		// Construct pion RTP packet from DiscordGo's type.
 		rtp := createPionRTPPacket(p)
-		err := speech.file.WriteRTP(rtp)
+		err := users[i].file.WriteRTP(rtp)
 		if err != nil {
 			fmt.Printf("failed to write to file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
 		}
 
-		speech.length += len(p.Opus)
-		speech.endTime = time.Now().Add(time.Millisecond * sentance_end_time_ms)
-		users[p.SSRC] = speech
+		// Increase the tracked length of the audio, used to prevent sending super short files which are typically bogus
+		users[i].length += len(p.Opus)
+
+		// Increase time till end of sentance
+		users[i].endTime = time.Now().Add(time.Millisecond * sentance_end_time_ms)
+
 		u_mut.Unlock()
 	}
 
+	// Stop goroutine
 	running = false
 
 	// Once we made it here, we're done listening for packets. Close all files
@@ -124,44 +160,56 @@ func handleVoice(s *discordgo.Session, chan_id string, discord_packets chan *dis
 	}
 }
 
-func send_speech_to_whisper(SSRC uint32) (WhisperResp, error) {
+func send_speech_to_whisper(ssrc uint32) (WhisperResp, error) {
 	var output WhisperResp = WhisperResp{}
 
+	// Create a default HTTP client, no timeout
 	client := http.Client{}
-	resp, err := UploadMultipartFile(&client, whisper_uri, "file", fmt.Sprintf("%d.ogg", SSRC))
+
+	// Upload the ogg file
+	log.Printf("Attempting to upload OGG file to Whsiper\n")
+	resp, err := UploadMultipartFile(&client, whisper_uri, "file", fmt.Sprintf("%d.ogg", ssrc))
 	if err != nil {
 		return output, err
 	}
+	log.Printf("Response: %s\n", resp.Status)
 
-	fmt.Println(resp.Status)
-
+	// Read the response body and then close it
 	bdy, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return output, err
 	}
 	resp.Body.Close()
 
+	// Convert raw bytes into Response Struct
 	err = json.Unmarshal(bdy, &output)
 	if err != nil {
 		return output, err
 	}
 
+	// Return response struct
 	return output, nil
 }
+
+// Stolen from Stackoverflow
+// https://stackoverflow.com/questions/20205796/post-data-using-the-content-type-multipart-form-data
 
 func UploadMultipartFile(client *http.Client, uri, key, path string) (*http.Response, error) {
 	body, writer := io.Pipe()
 	mwriter := multipart.NewWriter(writer)
 
+	// Generate new HTTP POST Request with a pipe as the body
 	req, err := http.NewRequest(http.MethodPost, uri, body)
 	if err != nil {
 		return nil, err
 	}
 
+	// Add a header for content type of multi-part form
 	req.Header.Add("Content-Type", mwriter.FormDataContentType())
 
 	errchan := make(chan error)
 
+	// Asynchronously read and "pipe" the file contents into the request body
 	go func() {
 		defer close(errchan)
 		defer writer.Close()
@@ -191,6 +239,7 @@ func UploadMultipartFile(client *http.Client, uri, key, path string) (*http.Resp
 		}
 	}()
 
+	// Start making the request
 	resp, err := client.Do(req)
 	merr := <-errchan
 
